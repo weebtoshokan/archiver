698984d1e54fc0d28e83aa79c39ccd1f89e0dbf2
Meachamp <34523297+Meachamp@users.noreply.github.com>
2018-08-08 21:39:00 -0400

    BoardWatcher code

---
+++ BoardWatcher.js
@@ -0,0 +1,53 @@
+const HashMap = require('hashmap')
+
+class BoardWatcher {
+	constructor(arr) {
+		this.boards = arr
+	}
+	
+	poll() {
+		this.boards.forEach((board) => {
+			board.requestAllThreads()
+			.then((obj) => {
+				this.processThreads(board, obj)
+			})
+		})
+		
+		
+		setTimeout(()=> this.poll(), 1000)
+	}
+	
+	start() {
+		this.poll()
+	}
+
+	processThreads(board, threadList) {
+		let oldThreads = board.getThreads()
+
+		let threadMap = new HashMap()
+
+		threadList.forEach((val, key) => {
+			threadMap.set(val.no, val)
+		})
+
+		oldThreads.forEach((val) => {
+			let exists = threadMap.has(val.no)
+			if(exists && threadMap.get(val.no).last_modified !== val.last_modified) {
+				// Thread updated
+				threadMap.remove(val.no)
+			}
+			
+			if(!exists) {
+				// Thread was deleted
+				threadMap.remove(val.no)
+			}
+		})
+
+		threadMap.forEach((val) => {
+			// New threads
+		})
+	}
+	
+}
+
+module.exports = BoardWatcher
