46e99f83542a8f0c4d003868f3aeb9e3b16ba94b
Meachamp <34523297+Meachamp@users.noreply.github.com>
2018-08-09 13:24:49 -0400

    Add database framework

--- database.js
+++ database.js
@@ -1,4 +1,6 @@
 const mysql = require('mysql2')
+const util = require('util')
+const HashMap = require('hashmap')
 
 
 class Database {
@@ -8,7 +10,7 @@ class Database {
         this.host = host
         this.port = port
         this.database = database
-
+        this.boardQueries = new HashMap();
         this.initDatabase()
     }
 
@@ -24,8 +26,34 @@ class Database {
         })
     }
 
-    insertPost() {
+    setupBoard(board) {
+        if(!boardQueries.has(board)) {
+            let queryObject = {}
 
+            queryObject.insert = util.format(
+                "INSERT INTO \"%s\"" +
+                "  (poster_ip, num, subnum, thread_num, op, timestamp, preview_orig, preview_w, preview_h, " +
+                "  media_filename, media_w, media_h, media_size, media_hash, media_orig, spoiler, deleted, " +
+                "  capcode, email, name, trip, title, comment, delpass, sticky, locked, poster_hash, poster_country, exif) " +
+                "    SELECT ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,? FROM DUAL " +
+                "    WHERE NOT EXISTS (SELECT 1 FROM \"%s\" WHERE num = ? AND subnum = ?)" +
+                "      AND NOT EXISTS (SELECT 1 FROM \"%s_deleted\" WHERE num = ? AND subnum = ?)", board)
+
+            this.boardQueries.set(board, queryObject)
+        } else {
+            throw new Error("Duplicate board initialized. Check your configs!")
+        }
+    }
+
+    getBoard(board) {
+        if(!this.boardQueries.has(board))
+            throw new Error('Board queries were not initialized!')
+
+        return this.board.get(board)
+    }
+
+    insertThread(board, thread) {
+        
     }
 }
 
