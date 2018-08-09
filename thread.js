b74dc6c3ec18622e412f671191023f877a85c72b
Meachamp <34523297+Meachamp@users.noreply.github.com>
2018-08-08 21:16:52 -0400

    thread class

---
+++ thread.js
@@ -0,0 +1,25 @@
+class Thread {
+	setPosts(posts) {
+		this.posts = posts
+	}
+
+	getPosts() {
+		return this.posts
+	}
+
+	getModified() {
+		return this.last_modified
+	}
+
+	setModified(modified) {
+		this.last_modified = modified
+	}
+
+	getPage() {
+		return this.page
+	}
+
+	setPage(page) {
+		this.page = page
+	}
+}
