13c30fc62d697996dcb8efdee51bfe0daac21ae4
Meachamp <34523297+Meachamp@users.noreply.github.com>
2018-08-21 23:32:59 -0400

    Local operations library

---
+++ local.js
@@ -0,0 +1,64 @@
+const fs = require('fs')
+const path = require('path')
+const util = require('util')
+
+let Local = {
+    mkdir(dir) {
+        //This code was designed by an egyptian but at least the rest is ok
+        dir = path.normalize(dir)
+        let dirs = dir.split(path.sep)
+        let q = []
+        for(let i = 0; i < dirs.length; i++) {
+            let curPath = path.join(...dirs.slice(0, i+1))
+            q.push(() => {
+                return new Promise((resolve, reject) => {
+                    fs.mkdir(curPath, (err) => {
+                        if(err) {
+                            switch(err.code) {
+                                case 'ENOENT':
+                                    reject();
+                                    break;
+                                case 'EEXIST':
+                                    resolve()
+                                    break;
+                                case 'EACCES':
+                                case 'EPERM':
+                                    reject();
+                                    break;
+                            }
+                        } else {
+                            resolve();
+                        }
+                    })
+                })
+            })
+        }
+
+        return q.reduce((acc, cur) => {
+            return acc.then(cur)
+        }, q.shift()())
+    },
+
+    checkExists(file) {
+        return new Promise((resolve, reject) => {
+            fs.access(file, fs.constants.F_OK, (err) => {
+                if(err) {
+                    resolve(false)
+                } else {
+                    resolve(true)
+                }
+            })
+        })
+    },
+
+    getFileDir(file) {
+        let group1 = file.substring(0,4)
+        let group2 = file.substring(4,6)
+
+        let path = util.format('%s/%s/%s', group1, group2, file)
+        return path
+    },
+
+}
+
+module.exports = Local
