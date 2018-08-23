cd612ccf8820ba4793098e5245e4ac44e6a7a6d0
Meachamp <34523297+Meachamp@users.noreply.github.com>
2018-08-23 11:29:31 -0400

    Add server entrypoint

---
+++ server.js
@@ -0,0 +1,23 @@
+const Board = require('./board')
+const BoardWatcher = require('./BoardWatcher')
+const Database = require('./database')
+const config = require('./config')
+
+let db = new Database(config.database.username, 
+    config.database.password, 
+    config.database.host, 
+    config.database.name, 
+    config.database.port)
+
+let boards = []
+config.boards.forEach((boardName) => {
+    let board = new Board(boardName, db)
+    boards.push(board)
+})
+
+let boardWatcher = new BoardWatcher(boards)
+boardWatcher.start()
+
+process.on('unhandledRejection', err => {
+    console.log(err);
+});
