ec27d780abd4a0365747c595022cac46de4c0300
Meachamp <34523297+Meachamp@users.noreply.github.com>
2018-08-25 00:02:54 -0400

    Implement address rotation

--- network.js
+++ network.js
@@ -1,6 +1,16 @@
+const config = require('./config')
+
+let curAddress = 0
+let len = config.address.list.length
+
 let Network = {
 	getNextAddress() {
-		return null
+		if(!config.address.rotate)
+			return null
+
+		let ret = config.address.list[curAddress]
+		curAddress = ++curAddress % len
+		return ret
 	}
 }
 
