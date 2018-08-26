1fda361d9f5cc1cb808242030fdebcf255dfed19
Meachamp <34523297+Meachamp@users.noreply.github.com>
2018-08-25 23:18:28 -0400

    Improve config defaults

--- config.js
+++ config.js
@@ -15,10 +15,10 @@ config.database.name = 'test'
 config.address.rotate = false // Enable this to rotate through a list of ip addresses for requests
 config.address.list = []
 
-config.ratelimit.concurrentImageRequests = 50 // Number of image requests that can be running at once
-config.ratelimit.concurrentThreadRequests = 20 // Number of thread requests that can be runnign at once
-config.ratelimit.imageDelay = 200 // Delay between image requests, in ms
-config.ratelimit.threadDelay = 1000 // Delay between thread requests, in ms
+config.ratelimit.concurrentImageRequests = 100 // Number of image requests that can be running at once
+config.ratelimit.concurrentThreadRequests = 20 // Number of thread requests that can be running at once
+config.ratelimit.imageDelay = 50 // Delay between image requests, in ms
+config.ratelimit.threadDelay = 500 // Delay between thread requests, in ms
 config.ratelimit.indexDelay = 10000 // Delay between thread index requests, in ms
 
 config.path.saveDir = 'tempConfig' // Where images are saved
